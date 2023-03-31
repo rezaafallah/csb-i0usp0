05534bee888081a7f527dbe6a532bbc4bc338ae9
Code Sandbox <hello@codesandbox.io>
2023-03-31 16:25:56 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.fa3fc44425065f33159900ca4011d7fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8daa53c206112b7f254a093aee4d4241",
+    "url": "https://rezaafallah.github.io/csb-i0usp0/index.html"
+  },
+  {
+    "revision": "74cdde6184acbf38e142",
+    "url": "https://rezaafallah.github.io/csb-i0usp0/static/css/2.4467c70b.chunk.css"
+  },
+  {
+    "revision": "3672080f49a828bc8e01",
+    "url": "https://rezaafallah.github.io/csb-i0usp0/static/css/main.88fd1d07.chunk.css"
+  },
+  {
+    "revision": "74cdde6184acbf38e142",
+    "url": "https://rezaafallah.github.io/csb-i0usp0/static/js/2.8cd85710.chunk.js"
+  },
+  {
+    "revision": "3672080f49a828bc8e01",
+    "url": "https://rezaafallah.github.io/csb-i0usp0/static/js/main.cf6a6295.chunk.js"
+  },
+  {
+    "revision": "d7d09b67ad2eae41b7b2",
+    "url": "https://rezaafallah.github.io/csb-i0usp0/static/js/runtime~main.f8794ee8.js"
+  }
+]);
